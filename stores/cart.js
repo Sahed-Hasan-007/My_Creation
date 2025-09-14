43c48e7d356dfd11200a15b018f20d90f81e3ec6
sahedhasan <skpappu34563456@gmail.com>
2025-09-14 16:10:51 +0600

    fix: update navigation links and component imports for eCommerce consistency

--- stores/cart.js
+++ stores/cart.js
@@ -18,7 +18,6 @@ export const useCartStore = defineStore("cart", {
 
       );
       if (existingItem) {
-
         existingItem.quantity += 1;
       } else {
         this.cartItems.push({ ...item, quantity: 1 });
